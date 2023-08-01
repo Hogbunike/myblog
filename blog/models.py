a4fdccbb78857a3f44acc61458a5186097ed75dc
Henry <hogbunike@gmail.com>
2023-08-01 01:33:08 +0100

    Added RichTextEditor

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField(max_length=500)
+    content = RichTextField(blank=True, null=True)
+    # content = models.TextField(max_length=500)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
